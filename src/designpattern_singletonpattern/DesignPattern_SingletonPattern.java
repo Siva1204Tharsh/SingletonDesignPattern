c969692f5957b61fc53b154642815f73bfcfa705
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-08-17 08:50:48 +0530

    First Commit

---
+++ src/designpattern_singletonpattern/DesignPattern_SingletonPattern.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpattern_singletonpattern;
+
+/**
+ *
+ * @author SIVA1201
+ */
+public class DesignPattern_SingletonPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
